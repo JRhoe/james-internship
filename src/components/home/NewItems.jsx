00b1195f7315aa186b436b02560b27e8c1eac3a8
James <jamesrhoe1@gmail.com>
2024-08-27 17:02:38 -0700

    updated explore page to use dynamic data

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -7,7 +7,7 @@ import Skeleton from "../UI/Skeleton";
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
-import ExpiryCountdown from "../UI/ExpiryCountdown";
+import NftDisplayCard from "../UI/NftDisplayCard";
 
 const NewItems = () => {
 
@@ -61,64 +61,10 @@ const NewItems = () => {
           :
           <OwlCarousel className='owl-theme' loop dots={false} items={4} margin={10} {...options} nav
           touchDrag={false}>
-          {data.map((elm, index) => (
-            // <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-            <div key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to={`/author/${elm.authorId}`}
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={elm.authorImage} alt={elm.authorId} />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                
-                {elm.expiryDate && <ExpiryCountdown
-                 expiryDate={elm.expiryDate}/>}
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <Link to={`/item-details/${elm.nftId}`}>
-                    <img
-                      src={elm.nftImage}
-                      className="lazy nft__item_preview"
-                      alt={elm.nftId}
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>{elm.title}</h4>
-                  </Link>
-                  <div className="nft__item_price">{elm.price} ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>{elm.likes}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+          {data.map((elm) => (
+            <NftDisplayCard
+            elm={elm}
+            isHome={true}/>
           ))}
           </OwlCarousel>
           }
